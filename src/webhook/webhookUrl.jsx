d30dc384e9a2da84826301186316c5e4f16bb63f
masayan1126 <masa199311266@gmail.com>
2020-11-22 18:43:23 +0900

    webhook urlを修正

--- src/webhook/webhookUrl.jsx
+++ src/webhook/webhookUrl.jsx
@@ -1,2 +1,2 @@
 export const webhookUrl =
- "https://hooks.slack.com/services/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";
+ "https://hooks.slack.com/services/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";
