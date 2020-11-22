2ec16e6502fade741a9f10b7a133c68aa93cdd03
masayan1126 <masa199311266@gmail.com>
2020-11-22 18:10:50 +0900

    webhook urlを修正

--- src/templates/Contact.jsx
+++ src/templates/Contact.jsx
@@ -11,10 +11,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import {db} from '../firebase/index';
 import { getUserId, getUsername, getUserProfileImage } from "../redux/users/selecotors";
 import { ScannerSharp } from '@material-ui/icons';
-import firebase from "firebase"
-import functions from '../firebase/index';
-import recipeDataset from "../dataset.json"
-// import sendMail from "../../functions/src/index";
 import  {webhookUrl} from '../webhook/webhookUrl';
 
 const useStyles = makeStyles((theme) => ({
